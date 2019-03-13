325b1e837fdab56bd417bbebe6898b5b8ded0460
Ed Dearment <ed787.ed@gmail.com>
2019-03-13 15:04:00 -0700

    fixed: .json,functions, css, lists

--- js/app.js
+++ js/app.js
@@ -1,6 +1,130 @@
 'use strict'
-var point = 0;
-function store() {
+var points = 0;
+var living = true;
+alert('I wanna play a game.');
+var questionZero = prompt('This is a game for your life. Do you wanna play?');
+questionZero = questionZero.toUpperCase();
+//console.log(questionZero);
+if(questionZero !== 'Y' && questionZero !== 'YES') {
+    document.location.reload();
+    //console.log(questionZero);
+}
+else{
+    alert('Excelent I knew you would see it my way!');
+    
+    var questionOne = prompt('Has Ed finished the Harry Potter series?');
+    questionOne = questionOne.toUpperCase();
+    console.log(questionOne);
+    if (questionOne === 'YES' || questionOne === 'Y') {
+        points--;
+        console.log(points);
+    }
+    else {
+        points++;
+        console.log(points);
+    }
+
+    var questionTwo = prompt('Has Ed ever done a triathalon?');
+    questionTwo = questionTwo.toUpperCase();
+    console.log(questionTwo);
+    if (questionTwo === 'YES' || questionTwo === 'Y') {
+        points--;
+        console.log(points);
+        }
+    else {
+        points++;
+        console.log(points);
+    }
+
+    var questionThree = prompt('Ed has swam non-stop for one hour?');
+    questionThree = questionThree.toLocaleUpperCase();
+    console.log(questionThree);
+    if (questionThree === 'NO' || questionThree === 'N') {
+        points--;
+        console.log(points);
+    }
+    else{
+        points++;
+    }
+
+    var questionFour = prompt('Ed has a favorite microbe?');
+    questionFour = questionFour.toUpperCase();
+    console.log(questionFour);
+    if(questionFour === 'NO' || questionFour === 'N'){
+        points--;
+        console.log(points);
+    }
+    else{
+        points++;
+        console.log(points);
+    }
+
+    var questionFive = prompt('Ed has seen the Blair Witch Project');
+    questionFive= questionFive.toUpperCase();
+    console.log(questionFive);
+    if(questionFive ==='YES' || questionFive === 'Y'){
+        points--;
+        console.log(points);
+    }
+    else{
+        points++;
+        console.log(points);
+    }
+}
+if(points < 0){
+    alert('You will now suffer my wraths!')
+    while (living){
+        alert('You find yourself in a room being interagated by a group of mobsters \n They are asking you if you or Jimmy stole took the money.')
+        var mobResponse = prompt('Do you A.Blame Jimmy B.Say nothing C.Something Else?')
+        mobResponse = mobResponse.toLocaleUpperCase();
+
+        if(mobResponse !== 'B'){
+            alert('Wrong! The mobsters kill you');
+            living = false;
+        }
+        else{
+            /*
+            alert('You are now in another room with Rebecca. \n in front of you are five goblets one has poison the other has water.')
+            var max = 4;
+            var poision = Math.floor(Math.random * Math.floor(max) + 1);
+            var rebeccaAlive = true;
+            var rebeccaChioce = Math.floor(Math.random * Math.floor(max) + 1);
+            var goblet = [1,2,3,4,5]
+            console.log(poision);
+            var choice = prompt("Who drinks first? A.You B.Rebbecca");
+            var gobletChoice;
+            choice = choice.toUpperCase();
+            if (choice !== 'A'){
+                while (rebeccaAlive === true && living === true) {
+
+                }
+            }
+            else{
+                prompt("")
+            }*/
+            alert('You are now confronted by the mighty Cthulhu \n he gives you three choices')
+            var deathChoice = prompt('Do you A.Choose to die B.Slowly lose your mind C.Walk away')
+            deathChoice = deathChoice.toUpperCase();
+            if(deathChoice === 'B'){
+                alert('You are no longer sane, and you gauge out your eyes, swallow your tongue bash your head into a wall \n Cthulu eats your body.')
+                living = false;
+            } 
+            else if (deathChoice === 'C'){
+                alert('You walk until you collapse from exhaustion Cthulu eats your tired body');
+                living = false;
+            }
+            else {
+                alert('Cthulu picks you up and eats you.');
+                living = false;
+            }
+        }
+    }
+}
+ if (living === false){
+     document.location.reload();
+ }
+
+/*function store() {
     var questionOne = document.getElementById('qOne').value.toUpperCase();
     var questionTwo = document.getElementById('qTwo').value.toUpperCase();
     var questionThree = document.getElementById('qThree').value.toUpperCase();
@@ -59,4 +183,4 @@ function store() {
         point = point.toString();
         document.getElementById('totalQuizPoints').innerHTML = "Total points are: " + point;
     }
-}
+}*/
